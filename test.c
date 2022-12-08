7defa2ec92944b9b9c7f4dd000e7a6617829fb9c
Yaksher <yaksher.git@gmail.com>
2022-12-08 03:39:50 -0800

    Add fibonacci test.

--- test.c
+++ test.c
@@ -6,10 +6,16 @@ ASYNC(intptr_t, square, intptr_t, arg) {
     return arg * arg;
 }
 
+ASYNC(intptr_t, fibonacci, intptr_t, n) {
+    if (n <= 1) {
+        return n;
+    }
+    return (intptr_t) AWAIT(fibonacci(n - 1)) + (intptr_t) AWAIT(fibonacci(n - 2));
+}
+
 int main() {
     async_init(0);
-    async_handle *handle = square(5);
-    printf("%d\n", (intptr_t) async_await(handle));
+    printf("%ld\n", (intptr_t) AWAIT(fibonacci(10)));
     async_close();
     return 0;
 }
