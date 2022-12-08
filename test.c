b4209fadc444bea27192028d15a4d2697de1366b
Yaksher <yaksher.git@gmail.com>
2022-12-08 05:03:02 -0800

    Add test for 0 argument async functions.

--- test.c
+++ test.c
@@ -13,9 +13,14 @@ ASYNC(intptr_t, fibonacci, intptr_t, n) {
     return AWAIT(intptr_t, fibonacci(n - 1)) + AWAIT(intptr_t, fibonacci(n - 2));
 }
 
+ASYNC(void *, fake_malloc) {
+    return malloc(100);
+}
+
 int main() {
     async_init(0);
     printf("%ld\n", AWAIT(intptr_t, fibonacci(10)));
+    printf("%p\n", AWAIT(void *, fake_malloc()));
     async_close();
     return 0;
 }
