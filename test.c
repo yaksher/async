9ec0adfa12189c0502eab67a37e93ed24386e546
Yaksher <yaksher.git@gmail.com>
2022-12-08 04:15:28 -0800

    Update tests to use new AWAIT syntax.

--- test.c
+++ test.c
@@ -10,12 +10,12 @@ ASYNC(intptr_t, fibonacci, intptr_t, n) {
     if (n <= 1) {
         return n;
     }
-    return (intptr_t) AWAIT(fibonacci(n - 1)) + (intptr_t) AWAIT(fibonacci(n - 2));
+    return AWAIT(intptr_t, fibonacci(n - 1)) + AWAIT(intptr_t, fibonacci(n - 2));
 }
 
 int main() {
     async_init(0);
-    printf("%ld\n", (intptr_t) AWAIT(fibonacci(10)));
+    printf("%ld\n", AWAIT(intptr_t, fibonacci(10)));
     async_close();
     return 0;
 }
